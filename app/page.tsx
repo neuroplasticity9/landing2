d18964277d5fca3670ca85bf0a3d72b9feb6696c
Neuroplasticity9 <vuquyettam@gmail.com>
2023-04-21 15:53:43 +0700

    Add resource

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,68 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+/* eslint-disable @next/next/no-sync-scripts */
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <header className='w-full bg-white justify-center flex'>
-        <div className='h-[71px] w-[1400px] justify-center flex items-center'>
-        <a className='mr-[88px]'><Image src={'images/logo.png'} alt={''} width={228} height={40}  /></a>
-          <nav className="flex items-center fontATM text-[17px]">
-            <a className='h-[100%] block uppercase' href='#'>Tin tức sự kiện</a><span className='px-[50px]'>|</span>
-            <a className='h-[100%] block uppercase' href='#'>Về chúng tôi</a><span className='px-[50px]'>|</span>
-            <a className='h-[100%] block uppercase' href='#'>Sản phẩm</a><span className='px-[50px]'>|</span>
-            <a className='h-[100%] block uppercase' href='#'>Đối tác</a><span className='px-[50px]'>|</span>
-            <a className='h-[100%] block uppercase' href='#'>Liện hệ</a>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+      <header className="w-full bg-white justify-center flex">
+        <div className="h-[71px] w-[1400px] justify-center flex items-center">
+          <a className="mr-[88px]">
+            <Image src={'images/logo.png'} alt={''} width={228} height={40} />
+          </a>
+          <nav className="flex items-center fontATM text-[17px] text-[#686868]">
+            <a className="h-[100%] block uppercase text-[#686868]" href="#">
+              Tin tức sự kiện
+            </a>
+            <span className="px-[50px]">|</span>
+            <a className="h-[100%] block uppercase text-[#686868]" href="#">
+              Về chúng tôi
+            </a>
+            <span className="px-[50px]">|</span>
+            <a className="h-[100%] block uppercase text-[#686868]" href="#">
+              Sản phẩm
+            </a>
+            <span className="px-[50px]">|</span>
+            <a className="h-[100%] block uppercase text-[#686868]" href="#">
+              Đối tác
+            </a>
+            <span className="px-[50px]">|</span>
+            <a className="h-[100%] block uppercase text-[#686868]" href="#">
+              Liện hệ
+            </a>
           </nav>
-        </div>        
+        </div>
       </header>
-      <section className='h-[590px] bg-slate-300'>
+      <section className="h-[590px] bg-slate-300">
+        {/* Slider main container */}
+        <div className="swiper mySwiper">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide"></div>
+            <div className="swiper-slide">Slide 2</div>
+            <div className="swiper-slide">Slide 3</div>
+            <div className="swiper-slide">Slide 4</div>
+            <div className="swiper-slide">Slide 5</div>
+            <div className="swiper-slide">Slide 6</div>
+            <div className="swiper-slide">Slide 7</div>
+            <div className="swiper-slide">Slide 8</div>
+            <div className="swiper-slide">Slide 9</div>
+          </div>
+          <div className="swiper-pagination" />
+        </div>
+
 
       </section>
+
       <main>
         <div>11111111</div>
       </main>
+
+      <Script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></Script>
+      <Script src="js/slide.js" ></Script>
     </>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   output: 'export',
   experimental: {
     appDir: true,
-  },
+  }
 }
 
 module.exports = nextConfig
