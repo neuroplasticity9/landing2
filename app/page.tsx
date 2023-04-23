3e5641f8aa87489a95fca49e7e26c2486d72774b
Neuroplasticity9 <vuquyettam@gmail.com>
2023-04-23 15:10:38 +0700

    comeback to semantic

--- app/page.tsx
+++ app/page.tsx
@@ -1,86 +1,82 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/no-sync-scripts */
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import Script from 'next/script';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-      <header className="w-full bg-white xl:justify-center xl:flex">
-        <div className="xl:h-[3.698vw] xl:w-[72.917vw] justify-center xl:justify-between flex items-center h-[14.974vw] group">
+      <header className="myHeader">
+        <div className="headerInner group">
           <a className="block">
             <picture>
               <source media="(max-width: 1200px)" srcSet="img/logo-mobile.png" />
-              <img src="img/logo.png" alt="" width={228} height={40} className="xl:w-[11.875vw] xl:h-[2.083vw] w-[32.552vw] h-auto" />
+              <img src="img/logo.png" alt="" width={228} height={40} className="logo" />
             </picture>
           </a>
-          <section className="xl:h-auto xl:w-[45.313vw] xl:static h-[14.974vw] absolute left-0 top-0 z-50 group-[.activeMn]:h-[156.251vw] group-[.activeMn]:w-full group-[.activeMn]:bg-white">
+          <section className="navTop">
             {/* chỗ này menu humberg */}
-            <div className="xl:hidden h-[14.974vw] flex items-center">
-              <a className="space-y-[1.693vw] block p-[2.604vw]">
-                <span className="block w-[10.8vw] h-[1.432vw] bg-gray-600 rounded-full" />
-                <span className="block w-[10.8vw] h-[1.432vw] bg-gray-600 rounded-full" />
-                <span className="block w-[10.8vw] h-[1.432vw] bg-gray-600 rounded-full" />
+            <div className="menuHumber">
+              <a href='#' className='menuArea'>
+                <span className="menuHumberLine" />
+                <span className="menuHumberLine" />
+                <span className="menuHumberLine" />
               </a>
             </div>
             {/* chỗ này navtop */}
-            <nav className="hidden xl:flex items-center justify-between fontATM text-[17px] text-[#686868] flex-wrap xl:flex-nowrap xl:ml-0 group-[.activeMn]:flex">
+            <nav className="navLinkArea">
               <a
-                className="xl:h-[100%] xl:border-none flex uppercase text-[#686868] w-full xl:w-auto h-[6.836vw] items-center border-t-[1px] pl-[3.906vw] xl:pl-[0px]"
+                className="navLink"
                 href="#">
                 Tin tức sự kiện
               </a>
-              <span className="hidden xl:inline">|</span>
+              <span className="navSpe">|</span>
               <a
-                className="xl:h-[100%] xl:border-none flex uppercase text-[#686868] w-full xl:w-auto h-[6.836vw] items-center border-t-[1px] pl-[3.906vw] xl:pl-[0px]"
+                className="navLink"
                 href="#">
                 Về chúng tôi
               </a>
-              <span className="hidden xl:inline">|</span>
+              <span className="navSpe">|</span>
               <a
-                className="xl:h-[100%] xl:border-none flex uppercase text-[#686868] w-full xl:w-auto h-[6.836vw] items-center border-t-[1px] pl-[3.906vw] xl:pl-[0px]"
+                className="navLink"
                 href="#">
                 Sản phẩm
               </a>
-              <span className="hidden xl:inline">|</span>
+              <span className="navSpe">|</span>
               <a
-                className="xl:h-[100%] xl:border-none flex uppercase text-[#686868] w-full xl:w-auto h-[6.836vw] items-center border-t-[1px] pl-[3.906vw] xl:pl-[0px]"
+                className="navLink"
                 href="#">
                 Đối tác
               </a>
-              <span className="hidden xl:inline">|</span>
+              <span className="navSpe">|</span>
               <a
-                className="xl:h-[100%] xl:border-none flex uppercase text-[#686868] w-full xl:w-auto h-[6.836vw] items-center border-t-[1px] pl-[3.906vw] xl:pl-[0px]"
+                className="navLink"
                 href="#">
                 Liện hệ
               </a>
             </nav>
           </section>
           {/* chỗ này là home page facebook */}
-          <div className="xl:hidden absolute right-0 top-0 flex h-[14.974vw] items-center w-[21.354vw] justify-between mr-[2.604vw]">
+          <div className="mIconTop">
             <a href="" title="Homepage">
-              <img src="img/icon-home.png" alt="Homepage" className="w-[9.505vw] h-[9.505vw]" />
+              <img src="img/icon-home.png" alt="Homepage" />
             </a>
             <a href="" title="My Facebook">
-              <img src="img/icon-fb.png" alt="Facebook" className="w-[9.505vw] h-[9.505vw]" />
+              <img src="img/icon-fb.png" alt="Facebook" />
             </a>
           </div>
           {/* hộp thoại search */}
-          <div className="xl:flex hidden">
-            <div className="border-[1px] border-[#b03a48] h-[1.563vw] w-[7.813vw] rounded-lg flex items-center">
-              <input type="text" className="w-full h-full border-none outline-none rounded-lg pl-[8px]" placeholder="Tìm kiếm" />
+          <div className="searchArea">
+            <div className="searchBox">
+              <input type="text" className="searchInput" placeholder="Tìm kiếm" />
               <a href="#">
-                <img src="img/icon-search.png" alt="" className="w-[0.99vw] h-[0.99vw] block ml-[8px] mr-[18px]" />
+                <img src="img/icon-search.png" alt="" className="searchIcon" />
               </a>
             </div>
           </div>
         </div>
       </header>
-      <section className="xl:h-[30.729vw] h-[31.25vw] bg-slate-300">
+      <section className="slideMain">
         {/* Slider main container */}
         <div className="swiper mySwiper">
           <div className="swiper-wrapper">
@@ -97,12 +93,12 @@ export default function Home() {
 
       <main>
         {/* body part 1  */}
-        <section className="bgType1 relative flex justify-center w-full">
-          <div className="xl:w-[72.917vw]  xl:h-[37.5vw] xl:py-[7.292vw] py-[3.907vw] flex justify-between flex-wrap xl:flex-row flex-col-reverse overflow-hidden">
-            <div className="xl:w-[calc(100%-39.583vw)] justify-center xl:justify-start">
-              <header className="flex justify-center xl:justify-start">
-                <span className="xl:h-[2.448vw] xl:w-[1.302vw] xl:mr-[0.625vw] h-[47px] w-[25px] bg-[#b03a48] xl:block hidden"></span>
-                <h2 className="xl:h-[2.448vw] xl:px-[1.563vw] xl:text-[1.563vw] text-[2.056vw] px-[2.344vw] h-[3.255vw] flex items-center content-center flex-wrap bg-[#b03a48]  text-[#fff] uppercase fntPBold">
+        <section className="area1Box bgType1">
+          <div className="area1Inner">
+            <div className="area1">
+              <header className="area1Header">
+                <span className="areaIcon"></span>
+                <h2 className="areaHead">
                   Tin tức - sự kiện
                 </h2>
               </header>
