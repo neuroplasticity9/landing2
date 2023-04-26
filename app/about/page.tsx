68f2640fa577c30ec4aee1ac9dfbd3654fd7191d
Neuroplasticity9 <vuquyettam@gmail.com>
2023-04-26 11:20:16 +0700

    fix all error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,48 +16,45 @@ export default function Page() {
                 <h1 className="aboutDtPage fntPBold">About us</h1>
                 <h2 className="aboutDt_Title fntPRegular">Skin Rosemary</h2>
                 <div className="aboutDt_content1 fntPRegular">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                  ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel
-                  facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-                  suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus
-                  vel facilisis.
+                  Skin Rosemary là một sản phẩm chăm sóc da tuyệt vời, được làm từ thành phần chính là tinh dầu cây hương thảo (rosemary) thiên nhiên. Với công thức độc
+                  đáo và giàu dưỡng chất, Skin Rosemary có thể giúp làm sạch, dưỡng ẩm và tái tạo làn da của bạn, mang lại cho bạn một làn da tươi trẻ, mịn màng và sáng
+                  bóng. Không những thế, Skin Rosemary còn có khả năng làm giảm sự xuất hiện của mụn trứng cá, giảm bã nhờn và ngăn ngừa sự lão hóa của da. Ngoài ra, sản
+                  phẩm còn có tác dụng làm se nhỏ lỗ chân lông, giúp da trở nên mịn màng và đều màu hơn. Đặc biệt, Skin Rosemary là sản phẩm được làm hoàn toàn từ thiên
+                  nhiên, không chứa các chất hóa học độc hại hay gây kích ứng cho da, vì vậy bạn có thể yên tâm sử dụng mà không lo gây hại cho làn da của mình.
                 </div>
               </div>
             </header>
             <section className="aboutDt_content2 fntPRegular">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.{' '}
-              </p>
-              <br />
-              <br />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.{' '}
-              </p>
+              Ngoài tinh dầu hương thảo, Skin Rosemary còn bổ sung các thành phần tự nhiên khác như tinh dầu bạc hà, tinh dầu chanh và chiết xuất từ rau má, tất cả đều có
+              tác dụng làm dịu da, tăng cường độ ẩm và cải thiện độ đàn hồi cho da. Skin Rosemary được thiết kế dạng sữa rửa mặt, độ pH cân bằng giúp làm sạch da hiệu quả
+              mà không gây khô và căng da. Sản phẩm cũng rất dễ sử dụng, chỉ cần lấy một lượng nhỏ và massage nhẹ nhàng lên mặt, sau đó rửa sạch bằng nước. Với công nghệ
+              sản xuất tiên tiến và chất lượng được kiểm định, Skin Rosemary đã trở thành một sản phẩm được nhiều người tin dùng và đánh giá cao trên thị trường chăm sóc
+              da. Nếu bạn đang tìm kiếm một sản phẩm làm sạch và chăm sóc da hiệu quả, Skin Rosemary sẽ là sự lựa chọn tuyệt vời cho bạn. Ngoài việc cải thiện chất lượng
+              da, Skin Rosemary còn có tác dụng giảm stress và cải thiện tâm trạng. Hương thảo được biết đến là một loại thảo mộc có tác dụng giảm căng thẳng và tạo cảm
+              giác sảng khoái, giúp bạn thư giãn sau một ngày dài làm việc. Điều đặc biệt ở Skin Rosemary là sản phẩm này không chỉ dành cho phụ nữ mà còn cho cả nam
+              giới. Với công thức và thành phần được thiết kế đặc biệt cho da nam, Skin Rosemary giúp làm sạch, cân bằng độ ẩm và ngăn ngừa lão hóa cho da nam, giúp da
+              trở nên khỏe mạnh và tươi trẻ hơn. Ngoài ra, Skin Rosemary còn có mùi thơm dịu nhẹ, không gây khó chịu cho người sử dụng và có thể giúp cải thiện cảm giác
+              tự tin của bạn khi tiếp xúc với người khác. Tóm lại, Skin Rosemary là một sản phẩm chăm sóc da tuyệt vời, được làm từ các thành phần tự nhiên và an toàn cho
+              da, mang lại nhiều lợi ích cho làn da và cả tâm trạng của bạn. Nếu bạn đang tìm kiếm một sản phẩm chăm sóc da hiệu quả và an toàn, hãy thử Skin Rosemary và
+              trải nghiệm sự khác biệt trên làn da của mình.
             </section>
             <div className="signalText fntFCong">Skin Rosemary</div>
-            <div className='aboutDt_content3'>
-              <div className='aboutDt_content3-detail'>
-                <img src="img/tong-giam-doc.webp" alt="" className='ceoImg' />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+            <div className="aboutDt_content3">
+              <div className="aboutDt_content3-detail">
+                <img src="img/tong-giam-doc.webp" alt="" className="ceoImg" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
+                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
+                ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
               </div>
             </div>
-            <div className='aboutDt_bottom'>
-              <img src="img/about1.webp" alt="" width="273" height="273" className='about1' />      
-              <img src="img/about2.webp" alt="" width="273" height="273" className='about2' />      
-              <img src="img/about3.webp" alt="" width="273" height="273" className='about3' />      
-              <img src="img/about4.webp" alt="" width="273" height="273" className='about4' />      
+            <div className="aboutDt_bottom">
+              <img src="img/about1.webp" alt="" width="273" height="273" className="about1" />
+              <img src="img/about2.webp" alt="" width="273" height="273" className="about2" />
+              <img src="img/about3.webp" alt="" width="273" height="273" className="about3" />
+              <img src="img/about4.webp" alt="" width="273" height="273" className="about4" />
             </div>
           </article>
         </section>
